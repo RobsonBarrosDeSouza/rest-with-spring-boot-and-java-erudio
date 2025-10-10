ce4e69602c88dac9d8054bb044335b1cac581e99
Robson Barros <robsonhx@hotmail.com>
2025-10-10 13:49:40 -0300

    primeiro commit

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/barros/Startup.java
@@ -0,0 +1,13 @@
+package br.com.barros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/barros/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.barros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
