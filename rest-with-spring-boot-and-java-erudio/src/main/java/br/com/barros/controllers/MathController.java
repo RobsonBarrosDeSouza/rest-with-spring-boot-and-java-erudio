dee591ac28b9a160ef1a1c595ea4a67c2eb5d9f4
Robson Barros <robsonhx@hotmail.com>
2025-10-14 14:03:45 -0300

    organizando classes

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/barros/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/barros/controllers/MathController.java
@@ -1,77 +1,56 @@
 package br.com.barros.controllers;
 
 import br.com.barros.exception.UnsupportedMathOperationException;
+import br.com.barros.math.SimpleMath;
+import br.com.barros.request.converters.NumberConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.OptionalDouble;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
-    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    @Autowired
+    private SimpleMath math;
+
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if(!isNumber(numberOne) || !isNumber(numberTwo)) throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-        return convertToDouble(numberOne)  + convertToDouble(numberTwo);
+        if(!NumberConverter.isNumber(numberOne) || !NumberConverter.isNumber(numberTwo)) throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
-    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    @GetMapping("/subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
-        if(!isNumber(numberOne) || !isNumber(numberTwo)) throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        if(!NumberConverter.isNumber(numberOne) || !NumberConverter.isNumber(numberTwo)) throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
+        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
-    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    @GetMapping("/multiplication/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable("numberOne")String numberOne, @PathVariable("numberTwo")String numberTwo)throws Exception{
-        if(!isNumber(numberOne) || !isNumber(numberTwo)) throw  new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        if(!NumberConverter.isNumber(numberOne) || !NumberConverter.isNumber(numberTwo)) throw  new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
+        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
      }
 
-    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    @GetMapping("/division/{numberOne}/{numberTwo}")
     public Double division(@PathVariable("numberOne")String numberOne, @PathVariable("numberTwo")String numberTwo)throws Exception {
-        if (!isNumber(numberOne) || !isNumber(numberTwo))
+        if (!NumberConverter.isNumber(numberOne) || !NumberConverter.isNumber(numberTwo))
             throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
-    @PostMapping("/average")
-    public Double average(@RequestBody List<Integer> numbers){
-        Double average = calculateAverage(numbers);
-        return average;
+    @GetMapping("/average/{numberOne}/{numberTwo}")
+    public Double average(@PathVariable("numberOne")String numberOne, @PathVariable("numberTwo")String numberTwo)throws Exception{
+        if (!NumberConverter.isNumber(numberOne) || !NumberConverter.isNumber(numberTwo))
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
+        return math.average(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
-    @RequestMapping("/squareroot/{number}")
+    @GetMapping("/squareroot/{number}")
     public Double squareRoot(@PathVariable("number") String number)throws Exception{
-        if(!isNumber(number))throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-      return  convertSquareRoot(number);
+        if(!NumberConverter.isNumber(number))throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
+        return math.squareRoot(NumberConverter.convertToDouble(number));
 
-    }
-
-    private Double convertSquareRoot(String number) {
-        if(convertToDouble(number) < 0.0) throw new UnsupportedMathOperationException("não é possivel calcular Raiz quadrada de um numero negativo!");
-        return Math.sqrt(convertToDouble(number));
-    }
-
-    private Double calculateAverage(List<Integer> numbers) {
-       if(numbers == null || numbers.isEmpty()){
-           return 0.0;
-       }
-       OptionalDouble mediaOptional = numbers.stream()
-               .mapToDouble(Integer::doubleValue)
-               .average();
-       return mediaOptional.orElse(0.0);
-    }
-
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
-        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Por favor, adicione um valor numerico!");
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-    }
-
-    private boolean isNumber(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/barros/math/SimpleMath.java
@@ -0,0 +1,32 @@
+package br.com.barros.math;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo) throws Exception {
+     return numberOne  + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo){
+      return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo){
+      return numberOne * numberTwo;
+    }
+
+    public Double division(Double numberOne, Double numberTwo){
+      return numberOne / numberTwo;
+    }
+
+    public Double average(Double numberOne, Double numberTwo){
+      return (numberOne + numberTwo)/ 2;
+    }
+
+    public Double squareRoot(Double number){
+      return Math.sqrt(number) ;
+    }
+
+}
